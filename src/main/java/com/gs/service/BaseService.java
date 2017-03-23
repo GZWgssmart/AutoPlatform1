0aa20fdb304bd0f3a83b3cf2a0918fd2019ae252
WangGenshen <847315251@qq.com>
2017-03-23 10:06:08 +0800

    修改BaseService增加泛型，修改Shiro查找指定角色的权限的sql代码

--- src/main/java/com/gs/service/BaseService.java
+++ src/main/java/com/gs/service/BaseService.java
@@ -2,9 +2,11 @@ package com.gs.service;
 
 import com.gs.base.IBase;
 
+import java.io.Serializable;
+
 /**
  * Created by Wang Genshen on 2017-03-22.
  */
-public interface BaseService extends IBase {
+public interface BaseService<PK extends Serializable, T> extends IBase {
 
 }
