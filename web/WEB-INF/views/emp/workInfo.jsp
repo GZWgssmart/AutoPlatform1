ea1478025b0efee3c3fcaa7b776a5ac876724187
jxJYY <jyyxzq@qq.com>
2017-04-24 15:25:28 +0800

    工单提交

--- web/WEB-INF/views/emp/workInfo.jsp
+++ web/WEB-INF/views/emp/workInfo.jsp
@@ -1,3 +1,5 @@
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -8,6 +10,9 @@
     <link rel="stylesheet" href="/static/css/bootstrap-table.css">
     <link rel="stylesheet" href="/static/css/select2.min.css">
     <link rel="stylesheet" href="/static/css/sweetalert.css">
+    <link rel="stylesheet" href="/static/css/table/table.css">
+    <link rel="stylesheet" href="/static/css/bootstrap-validate/bootstrapValidator.min.css">
+
     <link rel="stylesheet" href="/static/css/bootstrap-dateTimePicker/bootstrap-datetimepicker.min.css">
     <link rel="stylesheet" href="/static/css/bootstrap-dateTimePicker/datetimepicker.less">
 
@@ -17,37 +22,31 @@
 <%@include file="../backstage/contextmenu.jsp"%>
 
 <div class="container">
-    <div class="panel-body" style="padding-bottom:0px;"  >
+    <div class="panel-body">
         <!--show-refresh, show-toggle的样式可以在bootstrap-table.js的948行修改-->
         <!-- table里的所有属性在bootstrap-table.js的240行-->
-        <table id="table"
-               data-toggle="table"
-               data-toolbar="#toolbar"
-               data-url="/Order/queryAll"
-               data-method="post"
-               data-query-params="queryParams"
-               data-pagination="true"
-               data-search="true"
-               data-show-refresh="true"
-               data-show-toggle="true"
-               data-show-columns="true"
-               data-page-size="10"
-               data-height="700"
-               data-id-field="id"
-               data-page-list="[5, 10, 20]"
-               data-cach="false"
-               data-click-to-select="true"
-               data-single-select="true">
+        <table id="table" style="table-layout: fixed">
             <thead>
             <tr>
-                <th data-radio="true" data-field="status"></th>
-                <%--<th data-field="workId">工单编号</th>--%>
-                <th data-field="recordId">保养记录编号</th>
-                <th data-field="userId">工单指派用户编号</th>
-                <th data-field="workAssignTime" data-formatter="formatterDate">工单指派时间</th>
-                <th data-field="workCreatedTime" data-formatter="formatterDateTime">工单创建时间</th>
-                <th data-field="workStatus" data-formatter="formatterStatus">当前状态</th>
-                <th data-width="12%" data-formatter="openStatusFormatter">操作</th>
+                <th data-radio="true"></th>
+                <th data-width="100" data-formatter="recordId">
+                    保养记录编号
+                </th>
+                <th data-width="100" data-field="userId">
+                    指派工单编号
+                </th>
+                <th data-width="100" data-field="workAssignTime" data-formatter="formatterDate">
+                    工单指派时间
+                </th>
+                <th data-width="100" data-field="workCreatedTime"  data-formatter="formatterDateTime">
+                    工单创建时间
+                </th>
+                <th data-width="100" data-field="workStatus" data-formatter="formatterStatus">
+                    工单状态
+                </th>
+                <th data-width="100" data-field="inOutStatus" data-formatter="openStatusFormatter">
+                    操作
+                </th>
             </tr>
             </thead>
         </table>
@@ -67,24 +66,26 @@
 
 <!-- 添加弹窗 -->
 
-<div class="modal fade" id="add" aria-hidden="true" style="overflow:auto; ">
-    <div class="modal-dialog" style="height: auto; overflow:auto;">
-        <div class="modal-content" style="overflow:auto;">
-            <form class="form-horizontal" role="form" id="addForm" method="post">
+<div id="addWindow" class="modal fade" style="overflow:scroll" data-backdrop="static">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form class="form-horizontal" role="form" id="addForm">
                 <div class="modal-header" style="overflow:auto;">
                     <h4>请填写订单信息</h4>
                 </div>
                 <div class="form-group">
                     <label class="col-sm-3 control-label">保养记录编号：</label>
                     <div class="col-sm-7">
-                        <input type="text" name="recordId" placeholder="请输入保养记录编号" class="form-control">
+                        <select id="addRecordId" class="js-example-tags record" name="recordId" style="width:100%;">
+                        </select>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label class="col-sm-3 control-label">指派用户编号：</label>
                     <div class="col-sm-7">
-                        <input type="text" name="userId" placeholder="请输入工单指派用户编号" class="form-control">
+                        <select id="addUserId" class="js-example-tags user" name="userId" style="width:100%;">
+                        </select>
                     </div>
                 </div>
                 <div class="form-group">
@@ -99,10 +100,11 @@
                         <input type="date" name="workCreatedTime" id="addworkCreateTime"placeholder="请输入工单创建时间" class="form-control">
                     </div>
                 </div>
-                <div class="form-group">
+                <div class="modal-footer">
                     <div class="col-sm-offset-8">
                         <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-                        <button class="btn btn-sm btn-success" type="submit">保 存</button>
+                        <button id="addButton" type="button" onclick="addSubmit()" class="btn btn-primary">添加</button>
+                        <input type="reset" name="reset" style="display: none;"/>
                     </div>
                 </div>
             </form>
@@ -113,38 +115,38 @@
 
 
 <!-- 修改弹窗 -->
-<div class="modal fade" id="edit" aria-hidden="true" style="overflow:auto; ">
+<div class="modal fade" id="editWindow" aria-hidden="true" style="overflow:auto; ">
     <div class="modal-dialog" style="height: auto; overflow:auto;">
         <div class="modal-content" style="overflow:auto;">
-            <form class="form-horizontal" role="form"id="editForm" method="post">
+            <form class="form-horizontal" role="form"id="editForm">
                 <input type="hidden" name="workId" define="Order.workId"/>
-                <input type="hidden"name="workStatus" define="Order.workStatus">
+                <%--<input type="hidden"name="workStatus" define="Order.workStatus">--%>
                 <div class="modal-header" style="overflow:auto;">
                     <h4>请修改订单信息</h4>
                 </div>
                 <div class="form-group">
                     <label class="col-sm-3 control-label">保养记录编号：</label>
                     <div class="col-sm-7">
-                        <input type="text" name="recordId" placeholder="请输入保养记录编号" class="form-control">
+                        <input type="text" name="recordId" define="WorkInfo.recordId" placeholder="请输入保养记录编号" class="form-control">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label class="col-sm-3 control-label">指派用户编号：</label>
                     <div class="col-sm-7">
-                        <input type="text" name="userId" placeholder="请输入工单指派用户编号" class="form-control">
+                        <input type="text" name="userId" define="WorkInfo.userId" placeholder="请输入工单指派用户编号" class="form-control">
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-sm-3 control-label">工单指派时间：</label>
                     <div class="col-sm-7">
-                        <input type="date" name="workAssignTime" id="editwrokAssignTime" placeholder="请输入工单指派时间" class="form-control">
+                        <input type="date" name="workAssignTime" define="WorkInfo.workAssignTime" id="editwrokAssignTime" placeholder="请输入工单指派时间" class="form-control">
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-sm-3 control-label">工单创建时间：</label>
                     <div class="col-sm-7">
-                        <input type="date" name="workCreateTime" id="editworkCreateTime" placeholder="请输入工单创建时间" class="form-control">
+                        <input type="date" name="workCreatedTime" define="WorkInfo.workCreatedTime" id="editworkCreateTime" placeholder="请输入工单创建时间" class="form-control">
                     </div>
                 </div>
                 <div class="form-group">
@@ -162,15 +164,13 @@
 <!-- 删除弹窗 -->
 <div class="modal fade" id="del" aria-hidden="true">
     <div class="modal-dialog" >
-        <form action="/table/edit" method="post">
+        <form action="/Order/deleteById" method="post">
             <div class="modal-content">
                 <input type="hidden" id="delNoticeId"/>
                 <div class="modal-footer" style="text-align: center;">
                     <h2>确认删除吗?</h2>
-                    <button type="button" class="btn btn-default"
-                            data-dismiss="modal">关闭
-                    </button>
-                    <button type="sumbit" class="btn btn-primary" onclick="del()">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+                    <button type="sumbit" class="btn btn-primary" onclick="showDel()">
                         确认
                     </button>
                 </div>
@@ -204,12 +204,12 @@
 <script src="/static/js/jquery.formFill.js"></script>
 <script src="/static/js/select2/select2.js"></script>
 <script src="/static/js/sweetalert/sweetalert.min.js"></script>
-<script src="/static/js/form/jquery.validate.js"></script>
 <script src="/static/js/contextmenu.js"></script>
 <script src="/static/js/backstage/emp/workInFo.js"></script>
-<script src="/static/js/bootstrap-select/bootstrap-select.js"></script>
+<script src="/static/js/bootstrap-validate/bootstrapValidator.js"></script>
 <script src="/static/js/bootstrap-dateTimePicker/bootstrap-datetimepicker.min.js"></script>
 <script src="/static/js/bootstrap-dateTimePicker/locales/bootstrap-datetimepicker.zh-CN.js" charset="UTF-8"></script>
+<script src="/static/js/backstage/main.js"></script>
 
 </body>
 <script>
--- web/static/js/backstage/emp/workInFo.js
+++ web/static/js/backstage/emp/workInFo.js
@@ -1,3 +1,10 @@
+/**
+ *初始化表格
+ * @type {string}
+ */
+$(function () {
+    initTable("table","/Order/queryAll");
+});
 var contentPath='';
 $(function () {
     $('#table').bootstrapTable('hideColumn', 'wordId');
@@ -7,27 +14,82 @@ $(function () {
         }
     );
 
-    //绑定Ajax的内容
-    $.getJSON("/table/queryType", function (data) {
-        $("#addSelect").empty();//清空下拉框
-        $.each(data, function (i, item) {
-            $("#addSelect").append("<option value='" + data[i].id + "'>&nbsp;" + data[i].name + "</option>");
-        });
-    })
-//            $("#addSelect").on("select2:select",
-//                    function (e) {
-//                        alert(e)
-//                        alert("select2:select", e);
-//            });
-});
+}
 
+//添加
+function showAdd(){
+    //initDateTimePicker('addForm','workAssignTime');
+    $("#addWindow").modal('show');
+    $("#addButton").removeAttr("disabled");
+    validator('addForm');//初始化验证
+}
+/*表格验证*/
+function validator(formId) {
+    $('#' + formId).bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            recordId: {
+                message: '工单保养记录验证失败',
+                validators: {
+                    notEmpty: {
+                        message: '保养记录编号不能为空'
+                    }
+                }
+            },
+            userId: {
+                message: '指派用户验证失败',
+                validators: {
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    stringLength: {
+                        min: 1,
+                        max: 6,
+                        message: '用户名长度必须在1到6位之间'
+                    }
+                }
+            },
+            workAssignTime: {
+                message: '工单指派时间验证失败',
+                validators: {
+                    notEmpty: {
+                        message: '工单指派时间不能为空'
+                    }
+                }
+            },
+            workCreatedTime: {
+                message: '工单创建时间验证失败',
+                validators: {
+                    notEmpty: {
+                        message: '工单创建时间不能为空'
+                    }
+                }
+            }
+        }
+    })
+
+        .on('success.form.bv', function (e) {
+            if (formId == "addForm") {
+                formSubmit("/Order/add", formId, "addWindow");
+
+            } else if (formId == "editForm") {
+                formSubmit("/Order/edit", formId, "editWindow");
+
+            }
+        })
+
+}
 function showEdit(){
     var row =  $('table').bootstrapTable('getSelections');
     if(row.length >0) {
 //                $('#editId').val(row[0].id);
 //                $('#editName').val(row[0].name);
 //                $('#editPrice').val(row[0].price);
-        $("#edit").modal('show'); // 显示弹窗
+        $("#editWindow").modal('show'); // 显示弹窗
         var ceshi = row[0];
         $("#editForm").fill(ceshi);
     }else{
@@ -46,210 +108,93 @@ function showAdd(){
 function formatRepo(repo){return repo.text}
 function formatRepoSelection(repo){return repo.text}
 
+
+/*function getIdSelections() {
+    return $.map($table.bootstrapTable('getSelections'), function (row) {
+        return row.id
+    });
+}
+$remove.click(function () {
+    var ids = getIdSelections();
+    $table.bootstrapTable('remove', {
+        field: 'id',
+        values: ids
+    });
+    $remove.prop('disabled', true);
+})*/;
+
 function showDel(){
     var row =  $('table').bootstrapTable('getSelections');
     if(row.length >0) {
         $("#del").modal('show');
+        $('#table').bootstrapTable("delete");//重新加载网格数据
+        swal({
+            title:"",
+            text:"删除成功",
+            confirmButtonText:"确认",
+            type:"success"
+        })
     }else{
-        $("#tanchuang").modal('show');
+        swal({
+            title:"",
+            text:"删除失败",
+            confirmButtonText:"确认",
+            type:"fail"
+        })
+    }
+
+    // $.post(contentPath + "/Order/edit",
+    //     $("#del").serialize(),
+    //     function (data) {
+    //         if (data.result == "success") {
+    //             $("#edit").modal('hide'); // 关闭指定的窗口
+    //             $('#table').bootstrapTable("refresh"); // 重新加载指定数据网格数据
+    //             swal({
+    //                 title:"",
+    //                 text: data.message,
+    //                 type:"success"})// 提示窗口, 修改成功
+    //         } else if (data.result == "fail") {
+    //             //$.messager.alert("提示", data.result.message, "info");
+    //         }
+    //     }, "json"
+    // );
+}
+
+/*function checkAdd(){
+    var id = $('#addId').val();
+    var name = $('#addName').val();
+    var price = $('#addPrice').val();
+    var reslist=$("#addSelect").select2("data"); //获取多选的值
+    //alert(reslist.length)
+    if(id != "" && name != "" && price != ""){
+        return true;
+    }else{
+        var error = document.getElementById("addError");
+        error.innerHTML = "请输入正确的数据";
+        return false;
     }
 }
 
-// function checkAdd(){
-//     var id = $('#addId').val();
-//     var name = $('#addName').val();
-//     var price = $('#addPrice').val();
-//     var reslist=$("#addSelect").select2("data"); //获取多选的值
-//     //alert(reslist.length)
-//     if(id != "" && name != "" && price != ""){
-//         return true;
-//     }else{
-//         var error = document.getElementById("addError");
-//         error.innerHTML = "请输入正确的数据";
-//         return false;
-//     }
-// }
 
-//
-// function checkEdit() {
-//     $.post("/table/edit",
-//         $("#editForm").serialize(),
-//         function (data) {
-//             if (data.result == "success") {
-//                 $("#edit").modal('hide'); // 关闭指定的窗口
-//                 $('#table').bootstrapTable("refresh"); // 重新加载指定数据网格数据
-//                 swal({
-//                     title:"",
-//                     text: data.message,
-//                     type:"success"})// 提示窗口, 修改成功
-//             } else if (data.result == "fail") {
-//                 //$.messager.alert("提示", data.result.message, "info");
-//             }
-//         }, "json"
-//     );
-// }
-
-//前台验证
-$(document).ready(function () {
-    jQuery.validator.addMethod("isPhone", function (value, element) {
-        var length = value.length;
-        return this.optional(element) || (length == 11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value));
-    }, "请正确填写您的手机号码。");
-
-    $("#addForm").validate({
-        errorElement: 'span',
-        errorClass: 'help-block',
-
-        rules: {
-            recordId: {
-                required: true,
-                minlength: 2
-            },
-            userId: {
-                required: true,
-                minlength: 2
-            },
-            workAssignTime: {
-                required: true,
-                date: true
-            },
-            workCreateTime: {
-                required: true,
-                date: true
+function checkEdit() {
+    $.post("/table/edit",
+        $("#editForm").serialize(),
+        function (data) {
+            if (data.result == "success") {
+                $("#edit").modal('hide'); // 关闭指定的窗口
+                $('#table').bootstrapTable("refresh"); // 重新加载指定数据网格数据
+                swal({
+                    title:"",
+                    text: data.message,
+                    type:"success"})// 提示窗口, 修改成功
+            } else if (data.result == "fail") {
+                //$.messager.alert("提示", data.result.message, "info");
             }
-        },
-        messages: {
-            recordId: "请输保养记录编号",
-            userId: "请输入指派用户编号",
-            workAssignTime: "请输入工单指派时间",
-           workCreateTime: "请输入工单创建时间"
-        },
-        errorPlacement: function (error, element) {
-            element.next().remove();
-            element.after('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
-            element.closest('.form-group').append(error);
-        },
-        highlight: function (element) {
-            $(element).closest('.form-group').addClass('has-error has-feedback');
-        },
-        success: function (label) {
-            var el = label.closest('.form-group').find("input");
-            el.next().remove();
-            el.after('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
-            label.closest('.form-group').removeClass('has-error').addClass("has-feedback has-success");
-            label.remove();
-        },
-        submitHandler: function (form) {
-            $.post(contentPath+"/Order/add",
-                $("#addForm").serialize(),
-                function(data){
-                    if(data.result == "success"){
-                       $("#add").modal('hide');//关闭指定的窗口
-                       $('#table').bootstrapTable("refresh");//重新加载
-                        swal({
-                            title:"",
-                            text:data.messages,
-                            confirmButtonText:"确定",//提示按钮上的文本
-                            type:"success"})  //提示窗口 修改成功
-                    }else if(data.result == "fail"){
-                        swal({
-                            title:"",
-                            text:"添加失败",
-                            confirmButtonText:"确认",
-                            type:"fail"
-                        })
-                    }
-            },"json")
-        }
-    }),
-        $("#editForm").validate({
-            errorElement: 'span',
-            errorClass: 'help-block',
+        }, "json"
+    );
+}*/
 
-            rules: {
-                recordId: {
-                    required: true,
-                    minlength: 2
-                },
-                userId: {
-                    required: true,
-                    minlength: 2
-                },
-                workAssignTime: {
-                    required: true,
-                    date: true
-                },
-                workCreateTime: {
-                    required: true,
-                    date: true
-                }
-            },
-            messages: {
-                recordId: "请输保养记录编号",
-                userId: "请输入指派用户编号",
-                workAssignTime: "请输入工单指派时间",
-                workCreateTime: "请输入工单创建时间"
-            },
-            errorPlacement: function (error, element) {
-                element.next().remove();
-                element.after('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
-                element.closest('.form-group').append(error);
-            },
-            highlight: function (element) {
-                $(element).closest('.form-group').addClass('has-error has-feedback');
-            },
-            success: function (label) {
-                var el = label.closest('.form-group').find("input");
-                el.next().remove();
-                el.after('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
-                label.closest('.form-group').removeClass('has-error').addClass("has-feedback has-success");
-                label.remove();
-            },
-            submitHandler: function (form) {
-                $.post(contentPath+"/Order/update",
-                $("#editForm").serialize(),
-                    function(data){
-                        if(data.result == "success"){
-                            $("#edit").modal('hide');//隐藏
-                            $('#table').bootstrapTable("refresh");//重新加载网格数据
-                            swal({
-                                title:"",
-                                text:data.messages,
-                                confirmButtonText:"确定",
-                                type:"success"
-                            })
-                        }else if(data.result == "fail"){
-                            swal({
-                                title:"",
-                                text:"修改失败",
-                                confirmButtonText:"确定",
-                                type:"fail"
-                            })
-                        }
-                    },"json"
-                )
-            }
-        })
-});
-// //add日期插件
-// $('#addwrokAssignTime').datetimepicker({
-//     language: 'zh-CN',
-//     format: 'yyyy-mm-dd hh:ii'
-// });
-// $('#addworkCreateTime').datetimepicker({
-//     language: 'zh-CN',
-//     format: 'yyyy-mm-dd hh:ii'
-// });
-//
-// //edit日期插件
-// $('#editwrokAssignTime').datetimepicker({
-//     language: 'zh-CN',
-//     format: 'yyyy-mm-dd hh:ii'
-// });
-// $('#editworkCreateTime').datetimepicker({
-//     language: 'zh-CN',
-//     format: 'yyyy-mm-dd hh:ii'
-// });
+
 
 // 获取当前时间
 function getDate(id){
@@ -289,48 +234,6 @@ function formatterDate(value) {
         return year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
     }
 }
-
-
-//格式化带时分秒的时间值。
-function formatterDateTime(value) {
-    if (value == undefined || value == null || value == '') {
-        return "";
-    }
-    else {
-        var date = new Date(value);
-        var year = date.getFullYear().toString();
-        var month = (date.getMonth() + 1);
-        var day = date.getDate().toString();
-        var hour = date.getHours().toString();
-        var minutes = date.getMinutes().toString();
-        var seconds = date.getSeconds().toString();
-        if (month < 10) {
-            month = "0" + month;
-        }
-        if (day < 10) {
-            day = "0" + day;
-        }
-        if (hour < 10) {
-            hour = "0" + hour;
-        }
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        return year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
-    }
-}
-
-$("#editForm").submit(function(){
-    $(":submit",this).attr("disabled","disabled"); // 当修改表单提交时, 按钮不可点击
-});
-
-$("#addForm").submit(function(){
-    $(":submit",this).attr("disabled","disabled");// 当添加表单提交时, 按钮不可点击
-});
-
 
 $('#addworkAssignTime').datetimepicker({
     minView: "month", //选择日期后，不会再跳转去选择时分秒
