adf970dbf5ecaa3c34092f8da6e22460b7ef1fda
XMFBee <1626470874@qq.com>
2017-05-14 19:28:45 +0800

    修改UserRole的dao,service, serviceImpl

--- src/main/java/com/gs/service/UserRoleService.java
+++ src/main/java/com/gs/service/UserRoleService.java
@@ -1,6 +1,11 @@
 package com.gs.service;
 
+import com.gs.bean.User;
 import com.gs.bean.UserRole;
+import com.gs.common.bean.Pager;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
 *由CSWangBin技术支持
@@ -10,5 +15,15 @@ import com.gs.bean.UserRole;
 *@des 
 */
 public interface UserRoleService extends BaseService<String, UserRole>{
+
+    /**
+     * 模糊查询
+     */
+    public List<UserRole> blurredQuery(@Param("pager")Pager pager, @Param("userRole")UserRole userRole);
+
+    /**
+     * 模糊查询的记录数
+     */
+    public int countByBlurred(@Param("userRole")UserRole userRole, @Param("user")User user);
 
 }
